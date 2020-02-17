0946e45ca2f8577e7f2b263e3536b9fc1a338291
Minh Nguyen <minhnguyen@beeketing.net>
2020-02-17 22:58:53 +0700

    initiate project

---
+++ scripts/popup.js
@@ -0,0 +1,10 @@
+// document.addEventListener('DOMContentLoaded', function() {
+
+//     var displayAlerteButton = $('#displayAlert');
+//     displayAlerteButton.addEventListener('click', function() {
+//       alert('Welcome');
+//   }, false)});
+
+$('#displayAlert').on('click', function(){
+  alert('aa')
+})
